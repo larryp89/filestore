599138ba0906785b01a1bb8bfd5708e583b269fb
larryp89 <42215520+larryp89@users.noreply.github.com>
2024-12-11 15:08:00 +0000

    Add createFolder and addUser to repository

--- prisma/repository.js
+++ prisma/repository.js
@@ -3,16 +3,32 @@ const prisma = require("../db/db");
 
 async function addUser(email, password) {
   try {
-    await prisma.user.create({
+    const newUser = await prisma.user.create({
       data: {
         email: email,
         password: password,
       },
     });
     console.log("User Added to database!");
+    return newUser;
   } catch (err) {
     console.log("Error adding user to database:", err);
   }
 }
 
-module.exports = { addUser };
+async function createFolder(folderName, userID) {
+  try {
+    const newFolder = await prisma.folder.create({
+      data: {
+        folder_name: folderName,
+        userID: userID,
+      },
+    });
+    console.log("Successfully added folder");
+    return newFolder;
+  } catch (err) {
+    console.log("Error creating folder:", err);
+  }
+}
+
+module.exports = { addUser, createFolder };
