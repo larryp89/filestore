e142bce68d1ef534753f8d85cb7756c4ba0b83fb
larryp89 <42215520+larryp89@users.noreply.github.com>
2024-12-31 11:42:48 +0000

    Functioning tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js, ejs}"],
+  content: ["./views/**/*.ejs", "./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
