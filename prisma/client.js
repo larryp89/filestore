b515630ffcc65cb0f62f862c831d9a0728defc39
larryp89 <42215520+larryp89@users.noreply.github.com>
2025-01-02 16:11:16 +0000

    Modify schema

--- prisma/client.js
+++ prisma/client.js
@@ -1,3 +1,11 @@
 const { PrismaClient } = require("@prisma/client");
+
+require("dotenv").config();
+
+const isProduction = process.env.NODE_ENV === "production";
+process.env.DATABASE_URL = isProduction
+  ? process.env.PROD_DATABASE_URL
+  : process.env.LOCAL_DATABASE_URL;
+
 const prisma = new PrismaClient();
 module.exports = prisma;
