0a00119ad6d2fb9372ca1da73ea13447777a8ea3
larryp89 <42215520+larryp89@users.noreply.github.com>
2025-01-02 13:24:24 +0000

    Attempt changes with directory structure

--- api/index.js
+++ api/index.js
@@ -2,23 +2,23 @@ const express = require("express");
 const app = express();
 const path = require("node:path");
 const flash = require("connect-flash");
-const sessionMiddleware = require("./config/session.js");
-const passport = require("./config/passport");
-const errorHandler = require("./middleware/errorHandler.js");
-const indexRoutes = require("./routes/indexRoutes.js");
-const userRoutes = require("./routes/userRoutes.js");
-const folderRoutes = require("./routes/folderRoutes.js");
-const fileRoutes = require("./routes/fileRoutes.js");
+const sessionMiddleware = require("../config/session.js");
+const passport = require("../config/passport.js");
+const errorHandler = require("../middleware/errorHandler.js");
+const indexRoutes = require("../routes/indexRoutes.js");
+const userRoutes = require("../routes/userRoutes.js");
+const folderRoutes = require("../routes/folderRoutes.js");
+const fileRoutes = require("../routes/fileRoutes.js");
 
 // Configure the app to use EJS and specify the directory for EJS templates
-app.set("views", path.join(__dirname, "views")); // Tells Express to look for template files in the views directory.
+app.set("views", path.join(__dirname, "../views")); // Tells Express to look for template files in the views directory.
 app.set("view engine", "ejs"); // Sets EJS as the templating engine, allowing you to render EJS templates with the res.render method
 
 // Middleware to parse URL-encoded bodies
 app.use(express.urlencoded({ extended: true }));
 
 // Serve static fIiles from a public directory
-const assetsPath = path.join(__dirname, "public");
+const assetsPath = path.join(__dirname, "../public");
 app.use(express.static(assetsPath));
 
 // Set up session, which happens on every route request
